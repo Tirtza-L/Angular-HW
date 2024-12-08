ef68f265c40eb7131d1ede7c7bb9293f66d90729
Tirtza-L <3106244@gmail.com>
2024-12-08 14:18:41 +0200

    basic angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: "<h1>hello my-app</h1>",
+  selector: "app-root"
 })
+
 export class AppComponent {
-  title = 'start';
+  x: number = 5;
+
+  title: string = "hello!! this is startapp"
+
+  calc(){
+    return 0;
+  }
+
+  constructor()
+  {
+
+  }
 }
+
+
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.scss'
+// })
+// export class AppComponent {
+//   title = 'start';
+// }
